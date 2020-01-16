ccc6ab43c09b4af704e2db4d510da61b217612d9
JadeDufour <jade1dufour@gmail.com>
2020-01-16 16:58:42 -0500

    Exe1 : Initial commit
    Copied my code from the in-class exercise into my new project

--- CircleEater/js/script.js
+++ CircleEater/js/script.js
@@ -10,6 +10,22 @@ author, and this description to match your project!
 
 ******************/
 
+const PLAYER_SIZE_GAIN = 50;
+const PLAYER_SIZE_LOSS = 1;
+
+let player = {
+  x: 0,
+  y: 0,
+  size: 5,
+  maxSize: 60,
+  alive : true,
+  color : '#cccc55'
+}
+let prey = {
+  x: 0,
+  size : 55,
+  color: (250,150,100)
+}
 // preload()
 //
 // Description of preload
@@ -24,7 +40,9 @@ function preload() {
 // Description of setup
 
 function setup() {
-
+createCanvas(500,500);
+preyPosition();
+noCursor();
 }
 
 
@@ -34,4 +52,48 @@ function setup() {
 
 function draw() {
 
+  if (!player.alive) {
+     // By using "return" the draw() function exits immediately
+     return;
+   }
+
+  background(50,150,200);
+  updatePlayer();
+  drawPlayer();
+  displayFood();
+
+}
+
+
+
+function updatePlayer(){
+  player.x = mouseX;
+  player.y = mouseY;
+
+  player.size = constrain(player.size - PLAYER_SIZE_LOSS, 0, player.maxSize);
+  if (player.size === 0) {
+    player.alive = false;
+  }
+}
+
+function drawPlayer(){
+  push();
+  noStroke();
+  fill(player.color);
+  ellipse(player.x,player.y,player.size);
+  pop();
+}
+
+function displayFood(){
+  push();
+  ellipse(prey.x,prey.y,prey.size);
+  noStroke();
+  fill(prey.color);
+  pop();
+
+}
+
+function preyPosition(){
+  prey.x = random(0,width);
+  prey.y = random(0,height);
 }
---
+++ exercise1/js/script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+/********************************************************************
+
+Title of Project
+Author Name
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+
+window.addEventListener('load', setup);
+
+let numPixel = 1000;
+let pixelBGcolor = 'white';
+let pixelREVERTcolor = 'blue';
+let pixelDelay = 1000;
+let secondDelay = 500;
+let secondPixelcolor = "pink";
+    // setup
+    //
+    // Adds DIVs to the page along with event listeners that will allow
+    // then to change color on mouseover.
+function setup(){
+    // A loop that runs once per pixel we need
+  for (let i = 0; i < numPixel; i++) {
+    // Create a DIV and store it in a variable
+  let pixel = document.createElement('div');
+    // Add the 'pixel' class to the new element
+  pixel.setAttribute('class', 'pixel');
+    // Add a mouseover handler to the new element
+  pixel.addEventListener('mouseover', paint);
+    // Add the element to the body of the page
+  document.body.appendChild(pixel);
+  }
+}
+
+// paint
+//
+// Called by the mouseover event handler on each pixel. Changes
+// the pixel's color and sets a timer for it to revert
+function paint(e){
+  let pixel = e.target;
+  pixel.style.backgroundColor = pixelBGcolor;
+
+  // Set a timeout to call the reset function after a delay
+  // When we pass additional parameters (like 'pixel' below) they
+  // are passed to the callback function (resetPixel)
+  setTimeout(resetPixel, pixelDelay, pixel);
+}
+
+
+function resetPixel(pixel){
+  pixel.style.backgroundColor = pixelREVERTcolor;
+  setTimeout(reresetPixel, secondDelay, pixel);
+}
+
+
+function reresetPixel(pixel){
+  pixel.style.backgroundColor = secondPixelcolor;
+}
