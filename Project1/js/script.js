5495aeac3744850e56e6df94b3645aa9d63155c7
JadeDufour <jade1dufour@gmail.com>
2020-01-30 17:35:52 -0500

    P1: Commit bc I have to leave

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -9,6 +9,8 @@ This is the story of every messy person...
 
 *********************************************************************/
 let $bedSheets;
+let $pillow;
+let $chair;
 
 $(document).ready(setup);
 
@@ -16,8 +18,31 @@ $(document).ready(setup);
 function setup(){
 
   $bedSheets= $('.sheets');
+  $pillow = $('.pillows');
+  $chair = $('#officeChair');
+
+
+//the bed sheets cannot be put proprely on the bed and cannot leave the room
+//because of containment (why would they anyways)
+  $bedSheets.draggable({
+    containment: "parent",
+    //weird grid but that's the point
+    grid: [ 43, 26 ]
+  });
+
+//make the pillow draggable as well
+  $pillow.draggable({
+    grid: [ 143, 126 ]
+  });
+
+//The chair moves by itself
+  $chair.addEventListener('mouseover',moveChair);
+
+}
 
 
 
-
+function moveChair(){
+    $(this).animate({
+       margin: 400 });
 }
