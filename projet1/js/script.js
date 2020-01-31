bd98221150d163b23d8f9681c02ad0b54d5b3a64
JadeDufour <jade1dufour@gmail.com>
2020-01-31 18:01:04 -0500

    P1: added the faucet, the (future) mouth and the toothbrush
    + made the toothbrush draggable

--- projet1/js/script.js
+++ projet1/js/script.js
@@ -9,3 +9,25 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+
+let $water;
+let $tBrush;
+
+$(document).ready(setup);
+
+function setup(){
+
+$water= $('.faucet');
+$tBrush= $('.toothbrush');
+
+
+$tBrush.draggable({
+  containment: "parent",
+  axis: 'x',
+  //weird grid but that's the point
+  grid: [ 43, 26 ]
+});
+
+
+}
