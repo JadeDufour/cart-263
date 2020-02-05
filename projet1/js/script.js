eaeffefadf51dd46a955a02eba234c2419c071b0
JadeDufour <jade1dufour@gmail.com>
2020-02-05 15:52:04 -0500

    P1 Fixed some coding issues with Pippin
    -fixed the yelloying of the tooth
    -fixed the background not displaying

--- projet1/js/script.js
+++ projet1/js/script.js
@@ -1,9 +1,10 @@
+
 "use strict";
 
 /********************************************************************
 
 Epic dentistry
-Author Name
+Jade Dufour
 
 This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
@@ -18,101 +19,92 @@ let boringSFX;
 
 $(document).ready(setup);
 
-function setup(){
-boringSFX = new Audio('assets/sounds/elevatorMusic.wav');
-$water= $('.faucet');
+function setup() {
+  boringSFX = new Audio('assets/sounds/elevatorMusic.wav');
+  $water = $('.faucet');
 
-// the cursor is set to be a toothbrush
-$('body').css({
-   cursor: 'url(assets/images/toothbrush2.png), auto'
- });
-
-
-//the into dialog box, which serves no purpose
-$('#introDialog').dialog({
-    buttons:{
-      "Like no one ever was": one,
-      "To catch them is my real test" : two,
-      "To train them is my cause" : tree
-    }
+  // the cursor is set to be a toothbrush
+  $('body').css({
+    cursor: 'url(assets/images/toothbrush2.png), auto'
   });
 
 
+  //the into dialog box, which serves no purpose
+  $('#introDialog').dialog({
+    buttons: {
+      "Like no one ever was": one,
+      "To catch them is my real test": two,
+      "To train them is my cause": tree
+    }
+  });
+
+  //when the toothbrush hovers the faucets, the water stops and the sound too
+  $water.hover(function(event, ui) //on hover
+    {
+      $(this).attr('src', 'assets/images/source.png');
+    },
+    function() //on out
+    {
+      $(this).attr('src', 'assets/images/waterFaucet.gif');
+    })
 
 
-//when the toothbrush hovers the faucets, the water stops and the sound too
-$water.hover(function(event,ui) //on hover
-{
-   $(this).attr('src','assets/images/source.png');
-},
-function()                      //on out
-{
-  $(this).attr('src','assets/images/waterFaucet.gif');
-})
+  //------------------------------------------------------------------------------
+  //**Not final
+  //The teeth turn yellow with different delays with .animate
+  //
+
+  $('.teeth').each(goYellow);
 
 
+  //
+  // $('#teeth1 , #teeth3  , #teeth4').animate({
+  //   backgroundColor: '#F3E033'
+  // }, 10000);
+  //
+  // $('#teeth2 , #teeth6  , #teeth7').animate({
+  //   backgroundColor: '#F3E033'
+  // }, 18000);
+  //
+  // $(' #teeth8 , #teeth5').animate({
+  //   backgroundColor: '#F3E033'
+  // }, 30000);
 
 
+  //the point would be to make them go back to white when the mouse hovers them
 
 
-//------------------------------------------------------------------------------
-//**Not final
-//The teeth turn yellow with different delays with .animate
-//
-$('#teeth1 , #teeth3  , #teeth4').animate({
-  backgroundColor: '#F3E033'
-}, 10000);
+  $('.teeth').mouseenter(function() {
+    $(this).stop();
+    $(this).animate({
+      backgroundColor: 'white'
+    }, 100);
+  });
 
-$('#teeth2 , #teeth6  , #teeth7').animate({
-  backgroundColor: '#F3E033'
-}, 18000);
+  $('.teeth').on('mouseleave', goYellow);
 
-$(' #teeth8 , #teeth5').animate({
-  backgroundColor: '#F3E033'
-}, 30000);
-
-
-//the point would be to make them go back to white when the mouse hovers them
-
-
-$('#teeth1, #teeth2, #teeth3 , #teeth4 ,#teeth5, #teeth6, #teeth7,#teeth8').mouseenter(function () {
-
-  $('#teeth1, #teeth2, #teeth3 , #teeth4 ,#teeth5, #teeth6, #teeth7,#teeth8').animate({
-    backgroundColor: 'white'
-  }, 2000);
-
-
-
-
-//some code i found online
-      // var roll = null;
-      // var that = $(this);
-      //
-      //
-      //   if (that==="#trigger1"){roll = "$('#roll1r')";}
-      //   else if(that==="#trigger2"){roll ="$('#roll2r')";}
-      //   else if(that==="#trigger3"){roll = "$('#roll3r')";}
-      //   console.log(roll);
-      //   roll.fadeOut({
-      //       duration:300,
-      //      // fail: that.hide()
-      //   });
-    });
-//------------------------------------------------------------------------------
+  //------------------------------------------------------------------------------
 
 }
 
 
+function goYellow() {
+  $(this).animate({
+    backgroundColor: '#F3E033'
+  }, 1000 + Math.random() * 5000);
+}
 
-function one(){
+
+
+function one() {
   $(this).parent().effect('shake');
 }
 
-function two(){
+function two() {
   boringSFX.play();
 
 }
 
-function tree(){
+function tree() {
   $(this).parent().effect('puff');
 }
